ed5b6d30e69132183bd4f0eb242172e24ff04928
Roshan Punnoose <roshanp@gmail.com>
2013-04-03 14:13:49 -0400

    pento equals hashcode

--- src/main/java/pento/model/Pento.java
+++ src/main/java/pento/model/Pento.java
@@ -17,5 +17,20 @@ public final class Pento {
         return statements;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Pento pento = (Pento) o;
+
+        if (statements != null ? !statements.equals(pento.statements) : pento.statements != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return statements != null ? statements.hashCode() : 0;
+    }
 }
